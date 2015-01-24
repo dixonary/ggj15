901b7b4fe297b9af9ff2526595c81f06b60708f0
Gazok <jamiebayne@hotmail.com>
2015-01-24 23:49:02 +0000

    Allow node selection

--- source/EditGraph.hx
+++ source/EditGraph.hx
@@ -1,19 +1,21 @@
 package ;
 
+import flixel.FlxG;
 import flixel.group.FlxSpriteGroup;
+import flixel.util.FlxCollision;
 
 class EditGraph extends FlxSpriteGroup
 {
     var stages:Array<StageEdit> = [];
+    var selected:StageEdit;
 
     public function new(_stages:Array<Reg.Stage>)
     {
         super();
         var i:Int = 0;
         for (stage in _stages) {
-            trace("Hello!");
             ++i;
-            var s = new StageEdit(stage, i);
+            var s = new StageEdit(Reflect.copy(stage), i);
             stages.push(s);
             add(s);
         }
@@ -21,10 +23,23 @@ class EditGraph extends FlxSpriteGroup
 
     override public function update():Void
     {
+        var mousePos = FlxG.mouse.getWorldPosition();
+
         for (stage in stages) {
             if (stage.changed) {
                 stage.changed = false;
             }
+
+            if (FlxG.mouse.justPressed) {
+                if (FlxCollision.pixelPerfectPointCheck(
+                            cast(mousePos.x), cast(mousePos.y), stage.box)) {
+                    selected = stage;
+                }
+                stage.set_selected(false);
+            }
+        }
+        if (FlxG.mouse.justPressed && selected != null) {
+            selected.set_selected(true);
         }
 
         super.update();
--- source/StageEdit.hx
+++ source/StageEdit.hx
@@ -1,21 +1,70 @@
 package ;
 
 import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxPoint;
 import flixel.group.FlxSpriteGroup;
 import flixel.text.FlxText;
+import flixel.addons.display.FlxExtendedSprite;
 
 class StageEdit extends FlxSpriteGroup
 {
-    var stage:Reg.Stage;
-
+    inline static var size:Int = 30;
+    public var selected(default,set) = false;
+    public var stage(get,null):Reg.Stage;
+    public var box(default, null) = new FlxExtendedSprite();
+    var followMouse:Bool = false;
+    var mouseOffset:FlxPoint;
+    var defaultColor = 0xcc996699;
+    var selectedColor = 0xcccc8855;
     public var changed:Bool;
 
     public function new(_stage:Reg.Stage, hack_index:Int)
     {
-        super();
+        super((hack_index % 6) * size, Math.floor(hack_index /  6) * size);
         stage = _stage;
 
-        add(new FlxText((hack_index % 6) * 30, Math.floor(hack_index /  6) * 30,
-                    ""+stage.id, 20));
+        box.makeGraphic(size,size);
+        box.color = defaultColor;
+        add(box);
+        var text = new FlxText(0,0, ""+stage.id, 20) ;
+        text.alignment = "center";
+        add(text);
+    }
+
+    override public function update():Void
+    {
+        var mousePos = FlxG.mouse.getWorldPosition();
+        if (box.mouseOver && FlxG.mouse.justPressed) {
+            var pos = new FlxPoint(x,y);
+            followMouse = true;
+            mouseOffset = pos.subtractPoint(mousePos);
+        } else if (followMouse && FlxG.mouse.justReleased) {
+            followMouse = false;
+        }
+
+        if (followMouse) {
+            var newPos = mousePos.addPoint(mouseOffset);
+            x = newPos.x;
+            y = newPos.y;
+        }
+
+        super.update();
+    }
+
+    public function set_selected(v:Bool):Bool
+    {
+        if (v) {
+            box.color = selectedColor;
+        } else {
+            box.color = defaultColor;
+        }
+        selected = v;
+        return selected;
+    }
+
+    public function get_stage():Reg.Stage
+    {
+        return stage;
     }
 }
