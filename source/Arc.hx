3d803a0eef43ef3d9b7b08aee6751d472f16cb8c
alexbrainbox <alexbrainbox@gmail.com>
2015-01-25 11:11:45 +0000

    Arc remembers prev position

--- source/Arc.hx
+++ source/Arc.hx
@@ -5,6 +5,11 @@ import flixel.FlxSprite;
 using flixel.util.FlxSpriteUtil;
 
 class Arc extends FlxSprite {
+
+    var x1:Float;
+    var y1:Float;
+    var x2:Float;
+    var y2:Float;
     
     public function new(FromX:Float, FromY:Float, ToX:Float, ToY:Float) {
         super(FromX, FromY);
@@ -12,6 +17,14 @@ class Arc extends FlxSprite {
     }
 
     public function updatePos(FromX:Float, FromY:Float, ToX:Float, ToY:Float) {
+        if(FromX == x1 && FromY == y1 && ToX == x2 && ToY == y2)
+            return;
+        
+        x1 = FromX;
+        y1 = FromY;
+        x2 = ToX;
+        y2 = ToY;
+
         var dx = ToX-FromX;
         var dy = ToY-FromY;
         var length:Int = cast Math.sqrt(dx*dx+dy*dy);
